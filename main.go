7fffe66477aaa4ff70a67ee3445d75f3ebfadce3
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-10-28 20:40:27 +0700

    Loops in Go

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
+import "strings"
 
 func main(){
 	conferenceName := "Go Conference"
@@ -11,28 +12,36 @@ func main(){
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for{
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter Your First Name: ")
-	fmt.Scanln(&firstName)
+		fmt.Println("Enter Your First Name: ")
+		fmt.Scanln(&firstName)
 
-	fmt.Println("Enter Your Last Name: ")
-	fmt.Scanln(&lastName)
+		fmt.Println("Enter Your Last Name: ")
+		fmt.Scanln(&lastName)
 
-	fmt.Println("Enter Your Email: ")
-	fmt.Scanln(&email)
+		fmt.Println("Enter Your Email: ")
+		fmt.Scanln(&email)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scanln(&userTickets)
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scanln(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		
+				firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names %v\n", firstNames)
+	}
 }
